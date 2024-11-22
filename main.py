7a2ef72accbadaf02f783a058787f4e95e1a848e
NataKho <khorunzha.natalia96@gmail.com>
2024-11-22 16:21:05 +0100

    for i

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+list1 = [1,2,3,4]
+for i in list1:
+    print(i)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
